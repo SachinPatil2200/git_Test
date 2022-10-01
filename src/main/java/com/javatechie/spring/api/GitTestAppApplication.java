48eba9df73fcab6daf26cae0b243f06ab0a4e6b1
Admin <psachin2200@gmail.com>
2022-10-01 13:08:37 +0530

    First Commit

--- src/main/java/com/javatechie/spring/api/GitTestAppApplication.java
+++ src/main/java/com/javatechie/spring/api/GitTestAppApplication.java
@@ -2,6 +2,7 @@ package com.javatechie.spring.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GitTestAppApplication {
 
-	@PostMapping("/purchase")
-	public String purchase(@RequestBody Order order) {
-		return "Hi " + order.getUserName() + " order for " + order.getProductName() + " with amount "
-				+ order.getAmount() + " stored successfully...";
+	@PostMapping("/purchase/{}/{}/{}")
+	public String purchase(@PathVariable String uName ,@PathVariable String productName,@PathVariable String amount) {
+		return "Hi " + uName + " order for " + productName + " with amount "
+				+ amount + " stored successfully...";
 	}
 
 	public static void main(String[] args) {
